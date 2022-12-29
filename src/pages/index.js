1aaaf0d9dfb54804a648c78789af63eb91113d0c
Maija Barnett <maijabarnett@gmail.com>
2022-12-28 20:27:43 -0500

    amazon preview example

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,17 @@ const IndexPage = () => {
       <div className="home">
         {renderRichText(data.contentfulPage.body)}
       </div>
+      <iframe 
+        type="text/html" 
+        sandbox="allow-scripts allow-same-origin allow-popups" 
+        width="336" 
+        height="550" 
+        frameborder="0" 
+        allowfullscreen 
+        style={{ maxWidth: "100%" }}
+        src="https://read.amazon.com/kp/card?asin=B005AHO7PW&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_A9PNG5QRJ3RS558648XJ"
+      >
+        </iframe>
     </Layout>
   )
 }
