5bc1397ca9d9d3e68d94bf440c5f549a4cdda71c
Maija Barnett <maijabarnett@gmail.com>
2022-12-28 20:20:02 -0500

    change body font slightly to make darker

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,19 +4,19 @@ export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <link
       rel="preload"
-      href="/fonts/LexendDeca-ExtraLight.ttf"
+      href="/fonts/LexendDeca-Regular.ttf"
       as="font"
       type="font/ttf"
       crossOrigin="anonymous"
       key="lexenddeca"
     />,
     <link
-    rel="preload"
-    href="/fonts/DancingScript-Regular.ttf"
-    as="font"
-    type="font/ttf"
-    crossOrigin="anonymous"
-    key="dancing"
+      rel="preload"
+      href="/fonts/DancingScript-Regular.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+      key="dancing"
   />,
   ])
 }
