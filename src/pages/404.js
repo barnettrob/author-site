bafc75bad6a507357d2e1e98f15a30fa5bdc9178
Maija Barnett <maijabarnett@gmail.com>
2023-01-15 08:59:57 -0500

    custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,46 +1,29 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+    <Layout>
+      <h1 className="h3 mt-5">
+        Looking for something?
+      </h1>
+      <p>
+        Let's get you back{" "}<Link to="/">home</Link>.
       </p>
-    </main>
+      <div className="mt-5">
+        <StaticImage 
+          src="../images/rat404.jpg" 
+          alt={"page not found"}
+          className="lost-image"
+        />
+      </div>
+      <div className="fst-italic text-muted fs-6">
+        Image courtesy of Calista Barnett
+      </div>
+
+    </Layout> 
   )
 }
 
